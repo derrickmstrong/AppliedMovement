bcca09ee7fa50d0c8b99815f92ac6b14985f8eeb
Erron Dial <ErronDial@gmail.com>
2020-08-05 15:56:28 -0500

    Merge

--- src/server/routes/dbmethods.js
+++ src/server/routes/dbmethods.js
@@ -3,62 +3,58 @@ import db from '../db';
 
 const router = express.Router();
 
-router.get('/', async(req,res,next)=>{
-    
-})
-
 // GET /api/chirps/1 or GET /api/chirps
 router.get("/:id?", async (req, res, next) => {
-    const id = Number(req.params.id);
-    if (id) {
-      try {
-        const [chirp] = await db.chirps.one(id);
-        res.json(chirp);
-      } catch (error) {
-        next(error);
-      }
-    } else {
-      try {
-        const chirps = await db.chirps.all();
-        res.json(chirps);
-      } catch (error) {
-        next(error);
-      }
-    }
-  });
-  
-  // POST /api/chirps/
-  router.post("/", async (req, res, next) => {
-    const chirp = req.body;
+  const id = (req.params.id);
+  if (id) {
     try {
-      const result = await db.chirps.insert(chirp.userid, chirp.content);
-      res.json({ id: result.insertId });
+      const [chirp] = await db.chirps.one(id);
+      res.json(chirp);
     } catch (error) {
       next(error);
     }
-  });
-  
-  // PUT /api/chirps/1
-  router.put("/:id", async (req, res, next) => {
-    const id = Number(req.params.id);
-    const chirp = req.body;
+  } else {
     try {
-      await db.chirps.update(id, chirp.content);
-      res.json({ msg: "edited", id });
+      const chirps = await db.chirps.all();
+      res.json(chirps);
     } catch (error) {
       next(error);
     }
-  });
-  
-  // DELETE /api/chirps/1
-  router.delete("/:id", async (req, res, next) => {
-    const id = Number(req.params.id);
-    try {
-      await db.chirps.destroy(id);
-      res.json({ msg: "destroyed" });
-    } catch (error) {
-      next(error);
-    }
-  });
-  
-  export default router;
+  }
+});
+
+// POST /api/chirps/
+router.post("/", async (req, res, next) => {
+  const chirp = req.body;
+  try {
+    const result = await db.chirps.insert(chirp.userid, chirp.content);
+    res.json({ id: result.insertId });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// PUT /api/chirps/1
+router.put("/:id", async (req, res, next) => {
+  const id = Number(req.params.id);
+  const chirp = req.body;
+  try {
+    await db.chirps.update(id, chirp.content);
+    res.json({ msg: "edited", id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /api/chirps/1
+router.delete("/:id", async (req, res, next) => {
+  const id = Number(req.params.id);
+  try {
+    await db.chirps.destroy(id);
+    res.json({ msg: "destroyed" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
